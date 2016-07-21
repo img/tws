0964eb2b6f490f774d0914e4907a8cab16249c92
img <ian_green@persistent.com>
2016-07-21 18:41:19 +0100

    show how to construct an ArtifactRef for an embedded artefact, and use that ref to fetch its attributes

--- service.js
+++ service.js
@@ -28,6 +28,53 @@ function handleSelection(ref) {
   var service = url;
   gadgets.io.makeRequest(service, responseHandler, params);
   setStatus("Request has been sent to remote service", "running");
+
+
+// How to make an ArtifactRef from an existing one.  This is sample code that provides a workaround to
+// cases where the URI of the artefact is known, but where the API does not provide a means to
+// discover the ArtifactRef of that artefact.  (The example at had is an artefact which embeds other
+// artefacts; we can extract the URI from the xhtml, but there is no means to get the ArtifactRef of these
+// resources.
+
+    RM.Data.getAttributes(ref, [RM.Data.Attributes.PRIMARY_TEXT] , function (result) {
+        if(result.code !== RM.OperationResult.OPERATION_OK)
+        {
+            console.log("something went wrong");
+        } else {
+            result.data.forEach(function(attrs) {
+                var parser = new DOMParser();
+                // todo - parser doesn't like bnsp
+                var pt = "<div>"+attrs.values[RM.Data.Attributes.PRIMARY_TEXT].replace(new RegExp("&nbsp;", "g")," ")+"</div>";
+                var pt_node = parser.parseFromString(pt, "application/xml");
+                var embedded_anchors_list = pt_node.getElementsByClassName("embedded");
+                var embedded_anchors = [].slice.call(embedded_anchors_list, 0);
+                embedded_anchors.forEach(function(anchor) {
+
+                    // This construction is not supported  -  only for pilot/demo purposes!
+                    var embedded_ref = new RM.ArtifactRef(anchor.getAttribute('href'), ref.componentUri, undefined, ref.format);
+                    // Caveats:  (i) the artefact format of the embedded artefact is assumed to be the same as ref.  If this condition
+                    // is violated, the API will probably break. This means that only textual embedded artefacts can be access using
+                    // this workaround.
+
+                    // (ii) the embedded artefact must be in the same project area as that of ref.  Same warnings as above.
+                    // end of disclaimer
+
+                    console.log("fetching attributes for the embedded_ref: " + embedded_ref);
+                    RM.Data.getAttributes(embedded_ref, [RM.Data.Attributes.PRIMARY_TEXT] , function (result) {
+                        if(result.code !== RM.OperationResult.OPERATION_OK)
+                        {
+                            console.log("something went wrong");
+                        } else {
+                            result.data.forEach(function(attrs) {
+                                console.log("primary text of embedded artefact: ", attrs.values[RM.Data.Attributes.PRIMARY_TEXT]);
+                            });
+                        }
+                    });
+                });
+                console.log(pt);
+            });
+        }
+    });
 };
 
 /*
